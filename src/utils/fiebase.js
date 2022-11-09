7bd0710c18f98f617f58f401004a05422b338902
travis flake <tflake83@gmail.com>
2022-11-09 17:57:06 -0500

    firebase

---
+++ src/utils/fiebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCp1fg_BiXUaxbXSqk6L-ie-3HqFyuWGGA",
+  authDomain: "salon-website-8b2cb.firebaseapp.com",
+  projectId: "salon-website-8b2cb",
+  storageBucket: "salon-website-8b2cb.appspot.com",
+  messagingSenderId: "774572651574",
+  appId: "1:774572651574:web:45bb3f2b572044910d9497"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
