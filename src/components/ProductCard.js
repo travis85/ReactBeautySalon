4eaaa0d99e16005239d497a7242bea0615732490
travis flake <tflake83@gmail.com>
2022-11-09 17:57:26 -0500

    navigation change

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,21 @@
-import React,{useEffect, useState} from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {Link} from "react-router-dom";
 import { currencyFormatter } from '../utils/currencyFormatter'
 import '../index.css'
 
 function ProductCard({ cardData }) {
     const navigate = useNavigate()
-    const [state, setState] = useState('null')
-    const setCard = (card) => { setState(card) }
        
-        
 
     return (
+
         <ul>
             <div className='grid grid-cols-2 mt-4 sm:grid-cols-3' >
                 {cardData.map((card) => {
                     const photo = card.photo 
+
                     return (
                         
                         <li key={card.id} className='mr-2 mb-2 border-2 border-pink-500 w-fit '>
@@ -25,10 +23,12 @@ function ProductCard({ cardData }) {
                                 <img src={photo} id='cardPhoto' alt='image' />
                                 <Card.Body className='bg-fuchsia-200 h-36 '>
                                     <Card.Title className='text-purple-900'>{ card.title }</Card.Title>
-                                    <Card.Text className='text-purple-900'>{ currencyFormatter.format(card.price) } </Card.Text>
-                                    <Link onClick={navigate('/Booking', { state: { card: state } })}>
-                                        <Button variant="primary" className='absolute bottom-2 left-2'  onClick={()=> setCard(card)}> Book </Button>
-                                    </Link>
+                                    <Card.Text className='text-purple-900'>{currencyFormatter.format(card.price)} </Card.Text>
+                                    
+                                    <Button variant="primary" className='absolute bottom-2 left-2' onClick={() => 
+                                        navigate('/Booking', { state: { card: card } })}>
+                                        Book
+                                    </Button>
                                 </Card.Body>
                             </Card>       
                         </li>
@@ -42,6 +42,6 @@ function ProductCard({ cardData }) {
         
    
     )
- }
+}
 
  export default ProductCard
